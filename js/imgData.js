ccf9ff9a9b5b3ea15e8f968dc2e1594ebf7ce0b3
rdudley4 <github@rdudley.xyz>
2016-07-05 15:32:24 -0400

    Add animsition, adjust imgData.js
    - Change main to src for clarity.
    - Adjust path to src image
    - Add links to the images
    - Start lightbox implementation *WIP*

--- js/imgData.js
+++ js/imgData.js
@@ -1,72 +1,72 @@
 var imgDatabase = [
   {
-    main: '<img src="img/01.jpg">',
+    src: 'img/01.jpg',
     thumbnail: '<img src="img/Thumbnails/01.jpg">',
     title: 'Day in a Field',
     id: 1
   },
   {
-    main: '<img src="img/02.jpg">',
+    src: 'img/02.jpg',
     thumbnail: '<img src="img/Thumbnails/02.jpg">',
     title: 'Lake & Mountains',
     id: 2
   },
   {
-    main: '<img src="img/03.jpg">',
+    src: 'img/03.jpg',
     thumbnail: '<img src="img/Thumbnails/03.jpg">',
     title: 'Green Stuff',
     id: 3
   },
   {
-    main: '<img src="img/04.jpg">',
+    src: 'img/04.jpg',
     thumbnail: '<img src="img/Thumbnails/04.jpg">',
     title: 'Glaciers',
     id: 4
   },
   {
-    main: '<img src="img/05.jpg">',
+    src: 'img/05.jpg',
     thumbnail: '<img src="img/Thumbnails/05.jpg">',
     title: 'Canyon Mountains',
     id: 5
   },
   {
-    main: '<img src="img/06.jpg">',
+    src: 'img/06.jpg',
     thumbnail: '<img src="img/Thumbnails/06.jpg">',
     title: 'Interesting shrubs',
     id: 6
   },
   {
-    main: '<img src="img/07.jpg">',
+    src: 'img/07.jpg',
     thumbnail: '<img src="img/Thumbnails/07.jpg">',
     title: 'The Swamp',
     id: 7
   },
   {
-    main: '<img src="img/08.jpg">',
+    src: 'img/08.jpg',
     thumbnail: '<img src="img/Thumbnails/08.jpg">',
     title: 'The Beach',
     id: 8
   },
   {
-    main: '<img src="img/09.jpg">',
+    src: 'img/09.jpg',
     thumbnail: '<img src="img/Thumbnails/09.jpg">',
     title: 'Peaceful Pathway',
     id: 9
   },
   {
-    main: '<img src="img/10.jpg">',
+    src: 'img/10.jpg',
     thumbnail: '<img src="img/Thumbnails/10.jpg">',
     title: 'Mountain Waves',
     id: 10
   },
   {
-    main: '<img src="img/11.jpg">',
+    src: 'img/11.jpg',
     thumbnail: '<img src="img/Thumbnails/11.jpg">',
     title: 'Eye from the Sky',
     id: 11
   },
   {
-    main: '<img src="img/12.jpg">',
+    src: 'img/12.jpg',
     thumbnail: '<img src="img/Thumbnails/12.jpg">',
     title: 'Field of Stuffs',
     id: 12
--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 var currentImage;
+var $anchor = $( '#gallery a' );
+var $lightbox = $( '<div id="lightbox"></div>' );
 
-function assembleImage( image ) {
+$( 'body' ).append( $lightbox );
+
+/* When an image object is passed into the function..
+    1. Create a div to hold the image and title
+        1a. Create a span for the title
+    2. Get title from the objects title key
+        2a. Append the title and thumbnail to card
+    3. Append completed card to the page. */
+function assembleImage( imageObject ) {
     var $imageCard = $( '<div class="image-card"></div>' );
-    var $imageTitle = $('<span class="image-title"></span>');
-    
-    $imageTitle.text( image.title );
-    $imageCard.append( image.thumbnail );
-    $imageCard.append( $imageTitle );
+    var $imageTitle = $( '<span class="image-title"></span>' );
+    var $imageLink = $( '<a href="' + imageObject.src + '"</a>' );
+
+    $imageTitle.text( imageObject.title );
+    $imageCard.append( $imageLink );
+    $imageLink.append( imageObject.thumbnail );
+    $imageLink.append( $imageTitle );
     $( '#gallery' ).append( $imageCard );
 }
 
@@ -17,3 +29,21 @@ for ( var i = 0; i < imgDatabase.length; i++ ) {
   currentImage = imgDatabase[i];
   assembleImage( currentImage );
 }
+
+// Page loading animation
+$( '.animsition' ).animsition({
+  inClass: 'fade-in-down-lg',
+  outClass: 'fade-out-down-lg',
+  inDuration: 1000,
+  outDuration: 600,
+});
+
+$( '.image-card' ).click( function( e ) {
+  e.preventDefault();
+  console.log( 'Anchor has been clicked.' );
+  $lightbox.fadeIn( 'slow' );
+} );
+
+$lightbox.click( function() {
+  $lightbox.fadeOut( 'slow' );
+} );
